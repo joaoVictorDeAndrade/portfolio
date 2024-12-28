eca81547e59de0327f18e51706078bae89bf81fe
João Victor Pereira de Andrade <jv.andrade02@gmail.com>
2024-12-28 09:19:00 -0300

    add other logos

--- src/ui/components/IconsContainer.jsx
+++ src/ui/components/IconsContainer.jsx
@@ -6,18 +6,26 @@ import typescriptLogo from '/public/typescript.svg';
 import tailwindcssLogo from '/public/tailwindcss.svg';
 import nextJsLogo from '/public/nextjs.svg';
 import nodeLogo from '/public/node.svg';
+import awsLogo from '/public/aws.svg';
+import dockerLogo from '/public/docker.svg';
+import gitLogo from '/public/git.svg';
+import postgresLogo from '/public/postgres.svg';
 
 export function IconsContainer() {
   return (
-    <div className="grid grid-cols-5 gap-8">
+    <div className="grid grid-cols-6 items-center gap-8">
       <img src={reactLogo} alt="" />
       <img src={vueLogo} alt="" />
-      <img src={nextJsLogo} alt="" />
       <img src={nodeLogo} alt="" />
       <img src={jestLogo} alt="" />
-      <img src={javascriptLogo} alt="" className="col-start-2" />
+      <img src={javascriptLogo} alt="" />
       <img src={typescriptLogo} alt="" />
       <img src={tailwindcssLogo} alt="" />
+      <img src={nextJsLogo} alt="" />
+      <img src={awsLogo} alt="" />
+      <img src={dockerLogo} alt="" />
+      <img src={gitLogo} alt="" />
+      <img src={postgresLogo} alt="" />
     </div>
   );
 }
